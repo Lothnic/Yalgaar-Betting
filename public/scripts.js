517095c0b8c5aec49c9840992db0edc87cb64740
Mayank Joshi <24244@iiitu.ac.in>
2024-10-06 23:50:46 +0530

    Update scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
         addDefaultOption(matchSelect, "Choose Match");
 
         // Fetch ongoing matches from the backend
-        fetch('https://your-backend-url.com/ongoing-matches')
+        fetch('https://yalgaar-betting.vercel.app/ongoing-matches')
             .then(response => response.json())
             .then(data => {
                 console.log('Fetched ongoing matches:', data.matches);  // Log the fetched matches
